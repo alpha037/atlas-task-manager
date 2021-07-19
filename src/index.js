c73ed459243cd442405e0584d605f286f546c96c
alpha037 <subhronil.dutta@gmail.com>
2021-07-19 23:35:20 +0530

    Mod: IP address

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const app = require('./app');
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
+const address = process.env.IP || "0.0.0.0";
 
-app.listen(port, () => console.log(`Server is up on ${port}`));
+app.listen(port, address, () =>
+  console.log(`Server is up at ${address} on ${port}`)
+);
